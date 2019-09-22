ee6bf791aa79d5d68888e01e818923558449bdbd
Alyssa Yates <bosslys@icloud.com>
2019-09-22 19:29:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,26 +20,19 @@ def assign_rooms(attendees)
   array
 end
 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end 
+end
 
 
 
 
 
-
-
-
-
-#
-# def assign_rooms(attendees)
-#   num = 1
-#   array = []
-#   attendees.each do |name|
-#     array << "Hello, #{name}! You'll be assigned to room #{num}!"
-#     num += 1
-#   end
-#   array
-# end
-#
 #
 # def printer(attendees)
 #   batch_badge_creator(attendees).each do |badge|
